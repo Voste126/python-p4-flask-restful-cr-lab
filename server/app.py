3d1b900519f87221dbdaedaa2be8f7457db6fbdf
Voste126 <steveaustine126@gmail.com>
2023-09-22 15:05:31 +0300

    my work with GET RESTful API

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response,abort
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -17,11 +17,38 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plant_list = [
+            {
+                "id": plant.id,
+                "name": plant.name,
+                "image": plant.image,
+                "price": float(plant.price)  # Convert Decimal to float for JSON
+            }
+            for plant in plants
+        ]
+        response = make_response(jsonify(plant_list))
+        return response
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, plant_id):
+        plant = Plant.query.get(plant_id)
+
+        if not plant:
+            return abort(404, message="Plant not found")
+
+        plant_data = {
+            "id": plant.id,
+            "name": plant.name,
+            "image": plant.image,
+            "price": float(plant.price)  # Convert Decimal to float for JSON
+        }
+
+        response = make_response(jsonify(plant_data))
+        return response
+api.add_resource(PlantByID, '/plants/<int:plant_id>')       
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
